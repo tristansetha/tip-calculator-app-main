b93e2d9e176a2cbb42ca976d02e9c6a5fa2622c9
ts <tristansetha@gmail.com>
2021-08-28 11:56:03 -0700

    states fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,16 @@ let billInput = document.getElementById("bill-cost-input")
 let numberOfPeopleInput = document.getElementById("people-input")
 let tipAmountElement = document.getElementById("tip-amount")
 let totalAmountElement = document.getElementById("total-amount")
+let customTipInput = document.getElementById("customTipInput")
+
+let billInvalidLabel = document.getElementById("bill-invalid-label")
+let peopleInvalidLabel = document.getElementById("people-invalid-label")
+
+let btn5 = document.getElementById("btn5")
+let btn10 = document.getElementById("btn10")
+let btn15 = document.getElementById("btn15")
+let btn25 = document.getElementById("btn25")
+let btn50 = document.getElementById("btn50")
 
 let tipValue
 let billValue
@@ -12,37 +22,85 @@ let totaBillAmount
 
 billInput.addEventListener('input', handleBillValue)
 numberOfPeopleInput.addEventListener('input', handlePeopleValue)
-
+customTipInput.addEventListener('input', handleCustomTipInput)
 
 function handleBillValue(e) {
-	billValue = parseInt(e.target.value)
-	console.log(typeof billValue)
-	if (billValue && tipValue && peopleValue) {
-		totalTipAmount = (tipValue * billValue) / peopleValue
-		totaBillAmount = ((tipValue * billValue) + billValue) / peopleValue
-		tipAmountElement.textContent = totalTipAmount.toFixed(2)
-		totalAmountElement.textContent = totaBillAmount.toFixed(2)
+	if (!billInput.validity.valid) {
+		billInvalidLabel.classList.add("invalid-label-true")
+	} else {
+		billInvalidLabel.classList.remove("invalid-label-true")
+		billValue = parseInt(e.target.value)
+		console.log(typeof billValue)
+		if (billValue && tipValue && peopleValue) {
+			totalTipAmount = (tipValue * billValue) / peopleValue
+			totaBillAmount = ((tipValue * billValue) + billValue) / peopleValue
+			tipAmountElement.textContent = "$" + totalTipAmount.toFixed(2)
+			totalAmountElement.textContent = "$" + totaBillAmount.toFixed(2)
+		}
 	}
 }
 
-function handlePeopleValue(e) {
-	peopleValue = parseInt(e.target.value)
-	if (billValue && tipValue && peopleValue) {
-		totalTipAmount = (tipValue * billValue) / peopleValue
-		totaBillAmount = ((tipValue * billValue) + billValue) / peopleValue
-		tipAmountElement.textContent = totalTipAmount.toFixed(2)
-		totalAmountElement.textContent = totaBillAmount.toFixed(2)
-	}
+function handleCustomTipInput(e) {
+	let percentage = e.target.value / 100
+	handleTip(percentage)
 }
 
 function handleTip(val) {
 	tipValue = parseFloat(val)
+	if(tipValue === 0.05){
+		btn5.classList.add("tip-button-selected")
+		btn10.classList.remove("tip-button-selected")
+		btn15.classList.remove("tip-button-selected")
+		btn25.classList.remove("tip-button-selected")
+		btn50.classList.remove("tip-button-selected")
+	} else if (tipValue === 0.10){
+		btn5.classList.remove("tip-button-selected")
+		btn10.classList.add("tip-button-selected")
+		btn15.classList.remove("tip-button-selected")
+		btn25.classList.remove("tip-button-selected")
+		btn50.classList.remove("tip-button-selected")
+	} else if(tipValue === 0.15){
+		btn5.classList.remove("tip-button-selected")
+		btn10.classList.remove("tip-button-selected")
+		btn15.classList.add("tip-button-selected")
+		btn25.classList.remove("tip-button-selected")
+		btn50.classList.remove("tip-button-selected")
+	} else if (tipValue === 0.25){
+		btn5.classList.remove("tip-button-selected")
+		btn10.classList.remove("tip-button-selected")
+		btn15.classList.remove("tip-button-selected")
+		btn25.classList.add("tip-button-selected")
+		btn50.classList.remove("tip-button-selected")
+	} else if (tipValue === 0.50){
+		btn5.classList.remove("tip-button-selected")
+		btn10.classList.remove("tip-button-selected")
+		btn15.classList.remove("tip-button-selected")
+		btn25.classList.remove("tip-button-selected")
+		btn50.classList.add("tip-button-selected")
+	}
 	if (billValue && tipValue && peopleValue) {
 		totalTipAmount = (tipValue * billValue) / peopleValue
 		totaBillAmount = ((tipValue * billValue) + billValue) / peopleValue
-		tipAmountElement.textContent = totalTipAmount.toFixed(2)
-		totalAmountElement.textContent = totaBillAmount.toFixed(2)
+		tipAmountElement.textContent = "$" + totalTipAmount.toFixed(2)
+		totalAmountElement.textContent = "$" + totaBillAmount.toFixed(2)
 	}
+	console.log(tipValue)
+
+}
+
+function handlePeopleValue(e) {
+	if (!numberOfPeopleInput.validity.valid) {
+		peopleInvalidLabel.classList.add("invalid-label-true")
+	} else {
+		peopleValue = parseInt(e.target.value)
+		if (billValue && tipValue && peopleValue) {
+			totalTipAmount = (tipValue * billValue) / peopleValue
+			totaBillAmount = ((tipValue * billValue) + billValue) / peopleValue
+			tipAmountElement.textContent = "$" + totalTipAmount.toFixed(2)
+			totalAmountElement.textContent = "$" + totaBillAmount.toFixed(2)
+		}
+	}
+
 }
 
 function handleReset() {
@@ -54,16 +112,10 @@ function handleReset() {
 	totaBillAmount = ""
 	billInput.value = ""
 	numberOfPeopleInput.value = ""
-	tipAmountElement.textContent = ""
-	totalAmountElement.textContent = ""
+	tipAmountElement.textContent = "$0.00"
+	totalAmountElement.textContent = "$0.00"
 	console.log(billValue)
 }
 
 
-// if (tipValue){
-// 	console.log("hello tip")
-// }
 
-// add an eventlistener to each element
-// if all amounts exist update total
-// if tip amount exists update tip
